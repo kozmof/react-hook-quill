365db33e70551f13b6162d38b473344b5b16746e
Kozo Oeda <kozo.oeda@gmail.com>
2025-02-03 19:29:54 +0900

    Update `eslint.config.js`

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
           react: 'react'
         }
       },
-      external: Object.keys((pkg as any).dependencies || {})
+      external: Object.keys(pkg.dependencies || {})
     }
   }
 })
